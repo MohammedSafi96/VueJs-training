41860181d5a7b08f2003fa3ec167edf5282af236
m.safi <m.safi@mapletele.com>
2023-04-27 16:11:40 +0300

    initial push

---
+++ src/components/Modal.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="backdrop">
+<div class="modal">
+    <p>modal content</p>
+</div>
+    </div>        
+</template> 
+
+<style>
+.modal {
+    width: 400px;
+    padding: 20px;
+    margin: 300px auto;
+    margin-left: 40%;
+    background: white;
+    border-radius: 10px;
+}
+.backdrop {
+    top: 0;
+    position: fixed !important;
+    background: rgb(0,0,0,0.5);
+    width: 100%;
+    height: 100%;
+}
+</style>
