1f4fc20afa5f4ccf49be1df1bbd8bdac54cafc65
m.safi <m.safi@mapletele.com>
2023-04-30 12:50:48 +0300

    training v2

--- src/App.vue
+++ src/App.vue
@@ -5,25 +5,36 @@ import Modal from './components/Modal.vue'
 
 <template>
   <!-- <RouterView /> -->
- <div>  
-  <h1> {{ title }} </h1>
-  <h3>{{ subTitle }}</h3>
- </div>
+  <div>
+    <h1> {{ title }} </h1>
+    <h3>{{ subTitle }}</h3>
+  </div>
   <br>
- <div>
-  <input type="text" ref="name">
-  <button class="btn btn-primary" @click="handleClick">click me</button>
- </div>
+  <div>
+    <input type="text" ref="name">
+    <button class="btn btn-primary" @click="handleClick">click me</button>
 
- <Modal />
+  </div>
+<teleport to="#modals" v-if="showModal">
+  <Modal :arrayy="['ar1', 123]" theme="sale"  @close="toggleModal">
+    <template v-slot:links>
+      <a href="#">Sign up</a>
+      <a href="#">more info</a>
+    </template>      
+    <h1>Hi Ghazal</h1>
+    <p>some texr here!</p>
+  </Modal>
+</teleport>
+<button class="btn btn-primary" @click.alt="toggleModal">open modal (+alt)</button>
 </template>
 
-<script>  
+<script>
 export default {
-  data(){
+  data() {
     return {
       title: "vue js 3 training",
-      subTitle: ''
+      subTitle: '',
+      showModal: false
     }
   },
   components: {
@@ -32,6 +43,9 @@ export default {
   methods: {
     handleClick() {
       this.subTitle = this.$refs.name.value
+    },
+    toggleModal() {
+      this.showModal = !this.showModal
     }
   }
 }
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,25 +1,57 @@
 <template>
-    <div class="backdrop">
-<div class="modal">
-    <p>modal content</p>
-</div>
-    </div>        
+    <div class="backdrop" @click.self="closeModal">
+        <div class="modal" :class="{ sale: theme === 'sale' }">
+            <slot>defualt content</slot>
+            <span>{{ arrayy[0] }}</span>
+            <div class="actions">
+                <slot name="links"></slot>    
+            </div>
+        </div>
+    </div>
 </template> 
 
+<script>
+export default {
+    props: ['arrayy', 'theme'],
+    methods: {
+        closeModal(){
+            this.$emit('close')
+        }
+    }
+}
+
+
+</script>
 <style>
+.modal h1 {
+    color: green;
+}
+
+.modal.sale h1 {
+    color: rgb(251, 28, 28);
+}
+
 .modal {
     width: 400px;
     padding: 20px;
-    margin: 300px auto;
-    margin-left: 40%;
+    margin: 100px auto;
     background: white;
     border-radius: 10px;
 }
+
 .backdrop {
     top: 0;
     position: fixed !important;
-    background: rgb(0,0,0,0.5);
+    background: rgb(0, 0, 0, 0.5);
     width: 100%;
     height: 100%;
+}
+.modal .actions a {
+    color: #333;
+    padding: 8px;
+    border: 1px solid #eee;
+    border-radius: 4px;
+    text-decoration: none;
+    margin: 10px;
 }
 </style>
